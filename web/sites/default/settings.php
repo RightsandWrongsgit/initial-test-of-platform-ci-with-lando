c93e76352d4e4477d95104f9c6dd2764dd215061
Emmons Patzer <emofsnead@gmail.com>
2023-01-14 17:54:43 -0500

    Just cleans comment code attempts

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -27,92 +27,6 @@ $settings['file_scan_ignore_directories'] = [
 // local development.
 // $settings['hash_salt'] = 'change_me';
 
-// Detect Lando set environment variable ($env) to local
-//if (isset($_ENV['LANDO_INFO'])) { define('LANDO_INFO', json_decode($_ENV['LANDO_INFO'], TRUE));
-//}
-//if (defined('LANDO_INFO')) {
-//$env = 'local';
-//} else{
-//$env = getenv('PLATFORM_BRANCH'); 
-//}
-
-// Detect the environment ($env) from the Platform_Branch
-//if (file_exists($app_root . '/' . $site_path . '/settings.' . getenv('PLATFORM_BRANCH') . '.php')) {
-//  include $app_root . '/' . $site_path . '/settings.' . getenv('PLATFORM_BRANCH') . '.php';
-//}
-
-// Detect Platnformsh environment variable and grab the branch else set ($env) to local
-//if (isset($_ENV['LANDO_INFO'])) { define('LANDO_INFO', json_decode($_ENV['LANDO_INFO'], TRUE));
-//}
-
-// Enable the config-split definition for the environment
-//Switch ($env) {
-//if (defined('PLATFORM_BRANCH')) {
-//  $envir = getenv('PLATFORM_BRANCH');
-//  switch ($envir) {
-//    case 'develop':
-      // Environment indicator case 'develop':
-//      $config['environment_indicator.indicator']['bg_color'] = '#4caf50';
-//      $config['environment_indicator.indicator']['fg_color'] = '#000000';
-//      $config['environment_indicator.indicator']['name'] = 'develop';
-      // Config Split case 'develop':
-//      $config['config_split.config_split.develop']['status'] = TRUE;
-//      break;
-//    case 'staged':
-      // Environment indicator case 'staged':
-//      $config['environment_indicator.indicator']['bg_color'] = '#fff176';
-//      $config['environment_indicator.indicator']['fg_color'] = '#000000';
-//      $config['environment_indicator.indicator']['name'] = 'staged';
-      // Config Split case ‘staged’:
-//      $config['config_split.config_split.staged']['status'] = TRUE;
-//      break;
-//    case 'main':
-      // Environment indicator case 'main':
-//      $config['environment_indicator.indicator']['bg_color'] = '#ef5350';
-//      $config['environment_indicator.indicator']['fg_color'] = '#000000';
-//      $config['environment_indicator.indicator']['name'] = 'main';
-      // Config Split case ‘main’:
-//      $config['config_split.config_split.main']['status'] = TRUE;
-//      break;
-//    default:
-      //case 'local':
-//      // Environment indicator case 'local', white text on red:
-//      $config['environment_indicator.indicator']['bg_color'] = '#006600';
-//      $config['environment_indicator.indicator']['fg_color'] = '#ffffff';
-//      $config['environment_indicator.indicator']['name'] = 'local';
-//      // Config Split case ‘local’:
-//      $config['config_split.config_split.local']['status'] = TRUE;
-  // Environment indicator case 'default to sync', white text on purple:
-  //$config['environment_indicator.indicator']['bg_color'] = '#BF40BF';
-  //$config['environment_indicator.indicator']['fg_color'] = '#ffffff';
-  //$config['environment_indicator.indicator']['name'] = 'default to sync';
-//}
-//}
-
-// Detect Lando set environment variable ($env) to local
-//if (isset($_ENV['LANDO_INFO'])) { define('LANDO_INFO', json_decode($_ENV['LANDO_INFO'], TRUE));
-//}
-//if (defined('LANDO_INFO')) {
-//$env = 'local';
-//} else{
-//$env = getenv('PLATFORM_BRANCH'); 
-//}
-
-// Detect the environment ($env) from the Platform_Branch
-//if (file_exists($app_root . '/' . $site_path . '/settings.' . getenv('PLATFORM_BRANCH') . '.php')) {
-//  include $app_root . '/' . $site_path . '/settings.' . getenv('PLATFORM_BRANCH') . '.php';
-//}
-
-// Detect the environment ($env) from the Platform_Branch NOTE - DID NOT WORK
-//if (file_exists($app_root . '/' . $site_path . '/settings.' . getenv('PLATFORM_BRANCH') . '.php')) {
-//  include $app_root . '/' . $site_path . '/settings.' . getenv('PLATFORM_BRANCH') . '.php';
-//  $env = getenv('PLATFORM_BRANCH'); 
-//}
-
-// Detect Platnformsh environment variable and grab the branch else set ($env) to local
-//if (isset($_ENV['LANDO_INFO'])) { define('LANDO_INFO', json_decode($_ENV['LANDO_INFO'], TRUE));
-//}
-
 // Set up a config sync directory.
 //
 // This is defined inside the read-only "config" directory, deployed via Git.
@@ -123,12 +37,7 @@ $config['config_split.config_split.local']['status'] = FALSE;
 $config['config_split.config_split.develop']['status'] = FALSE;
 $config['config_split.config_split.staged']['status'] = FALSE;
 $config['config_split.config_split.main']['status'] = FALSE;
-// Detect Lando set environment variable ($env) to local - NOTE THIS APPEARS TO WORK
-//if (isset($_ENV['LANDO_INFO'])) { define('LANDO_INFO', json_decode($_ENV['LANDO_INFO'], TRUE));
-//}
-//if (defined('LANDO_INFO')) {
-//$env = 'local';
-//}
+
 // Detect the environment ($env) from the Platform_Branch
 if (isset($_ENV['PLATFORM_ENVIRONMENT'])) { define('PLATFORM_BRANCH', json_decode($_ENV['PLATFORM_BRANCH'], TRUE));
 }
