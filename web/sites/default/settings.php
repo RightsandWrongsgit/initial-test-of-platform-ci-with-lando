64dabcb4e2c20aebb03ea548754b8783dad94363
RightsandWrongsgit <emofsnead@gmail.com>
2025-03-29 16:38:44 -0400

    Updated to work on DDEV

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @file
- * Platform.sh example settings.php file for Drupal 9.
+ * Platform.sh example settings.php file for Drupal 10.
  */
 
 // Default Drupal settings.
@@ -19,6 +19,7 @@ $settings['file_scan_ignore_directories'] = [
   'bower_components',
 ];
 
+// THE FOLLOWING ITEMS UNTIL NOTED DIFFERENTLY ARE FROM DDEVs settings.php but NOT UPDATED HERE
 // The hash_salt should be a unique random value for each application.
 // If left unset, the settings.platformsh.php file will attempt to provide one.
 // You can also provide a specific value here if you prefer and it will be used
@@ -26,6 +27,49 @@ $settings['file_scan_ignore_directories'] = [
 // can configure a separate hash_salt in your settings.local.php file for
 // local development.
 // $settings['hash_salt'] = 'change_me';
+
+/**
+ * The default number of entities to update in a batch process.
+ *
+ * This is used by update and post-update functions that need to go through and
+ * change all the entities on a site, so it is useful to increase this number
+ * if your hosting configuration (i.e. RAM allocation, CPU speed) allows for a
+ * larger number of entities to be processed in a single batch run.
+ */
+# $settings['entity_update_batch_size'] = 50;
+
+/**
+ * Entity update backup.
+ *
+ * This is used to inform the entity storage handler that the backup tables as
+ * well as the original entity type and field storage definitions should be
+ * retained after a successful entity update process.
+ */
+# # $settings['entity_update_backup'] = TRUE;
+
+/**
+ * State caching.
+ *
+ * State caching uses the cache collector pattern to cache all requested keys
+ * from the state API in a single cache entry, which can greatly reduce the
+ * amount of database queries. However, some sites may use state with a
+ * lot of dynamic keys which could result in a very large cache.
+ */
+# $settings['state_cache'] = TRUE;
+
+/**
+ * Node migration type.
+ *
+ * This is used to force the migration system to use the classic node migrations
+ * instead of the default complete node migrations. The migration system will
+ * use the classic node migration only if there are existing migrate_map tables
+ * for the classic node migrations and they contain data. These tables may not
+ * exist if you are developing custom migrations and do not want to use the
+ * complete node migrations. Set this to TRUE to force the use of the classic
+ * node migrations.
+ */
+# $settings['migrate_node_migrate_type_classic'] = FALSE;
+// END OF SUPPRESSED DDEV OFFERED settings.php items NOT include in active.
 
 // Set up a config sync directory.
 //
@@ -39,11 +83,12 @@ $config['config_split.config_split.staged']['status'] = FALSE;
 $config['config_split.config_split.main']['status'] = FALSE;
 
 // Detect the environment ($env) from the Platform_Branch
-if (isset($_ENV['PLATFORM_ENVIRONMENT'])) { define('PLATFORM_BRANCH', json_decode($_ENV['PLATFORM_BRANCH'], TRUE));
-}
-if (defined('PLATFORM_BRANCH')) {
-  $env = getenv('PLATFORM_BRANCH'); 
-}
+// if (isset($_ENV['PLATFORM_ENVIRONMENT'])) { define('PLATFORM_BRANCH', json_decode($_ENV['PLATFORM_BRANCH'], TRUE));
+// }
+// if (defined('PLATFORM_BRANCH')) {
+  $env = getenv('PLATFORM_ENVIRONMENT'); 
+// }
+
 // Enable the config-split definition for the environment
 switch ($env) {
   case 'develop':
@@ -82,6 +127,11 @@ switch ($env) {
 // Config Split case ‘local’:
   $config['config_split.config_split.local']['status'] = TRUE;
   $config['stage_file_proxy.settings']['origin'] = `platform url -e main --primary --pipe`;
+}
+
+// Automatically generated include for settings managed by ddev.
+if (getenv('IS_DDEV_PROJECT') == 'true' && file_exists(__DIR__ . '/settings.ddev.php')) {
+  include __DIR__ . '/settings.ddev.php';
 }
 
 // Automatic Platform.sh settings.
